764dee18f19ff4a74a83341a9100f2fc8cdf0ee4
ivan <ivanchantf@gmail.com>
2024-06-14 13:10:18 +0800

    update

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ ScrollReveal().reveal('.home-contact p, .about-content',   {origin:'left'});
 
 /*---------------TypeJSs----------------------*/
 const typed= new Typed('.multiple-text',{
-    strings: [' Computer Science graduate'],
+    strings: [' Computer Science graduate','Web Developer'],
     typeSpeed:70,
     backSpeed:70,
     backDelay:1000,
